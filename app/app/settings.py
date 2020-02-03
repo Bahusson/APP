2621b75b86b7d873b5734bae97a1b520c92f8916
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-02-03 14:35:03 +0100

    Wstępna modyfikacja ustawień
    Przygotowanie do wrzucenia pod serwer w trybie dotychczas standardowym.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,16 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login_required',
+    'ajax',
+    'storages',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'login_required.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -77,8 +84,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Mu3kata!owieC',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -101,13 +112,41 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'boards.User'
+
+# CACHE bazodanowy. https://docs.djangoproject.com/en/2.2/topics/cache/
+# Przed użyciem stwórz tabelę w bazie danych za pomocą:
+# "python manage.py createcachetable"
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache_table',
+        'TIMEOUT': 10,
+        'OPTIONS': {
+            'MAX_ENTRIES': 2000,
+            'CULL_FREQUENCY': 2
+        }
+    }
+}
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+
+CACHE_MIDDLEWARE_SECONDS = 1
+
+CACHE_MIDDLEWARE_KEY_PREFIX = ''
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+# Dla sesji opartych na ciastkach:
+# "django.contrib.sessions.backends.signed_cookies"
+# Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -115,12 +154,107 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Moduł tłumaczeniowy, jak wszystko z MODELTRANSLATION w nazwie
+# https://django-modeltranslation.readthedocs.io/en/latest/
+# klasy są tak poustawiane, że dodanie lub usunięcie języka z settings.py
+# automatycznie powoduje ich dodanie/usunięcie wszędzie indziej.
+# TODO: Poprawić langjs.js żeby też był z tym zsynchronizowany.
+gettext = lambda s: s
+LANGUAGES = (
+    # Pierwszy jest zawsze defaultem chyba, że zrobisz override.
+    ('pl', gettext('Polish')),
+    ('en', gettext('English')),
+)
+
+LANGUAGE_COOKIE_AGE = 31449600  # Około rok ważności
+# Ustaw None jeśli chcesz, żeby się zerowały po każdym wyłączeniu przeglądarki.
+
+LANGUAGE_COOKIE_NAME = 'app_language'  # Nazwa ciacha językowego
+
+LANGUAGE_COOKIE_PATH = '/'  # Domyślna ścieżka ciastków.
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'pl'  # Tu możesz zmienić default language.
+
+MODELTRANSLATION_LANGUAGES = ('pl', 'en')
+
+# W ten sposób zachowają sie języki jak nie znajdzie się jakiegoś w bazie.
+MODELTRANSLATION_FALLBACK_LANGUAGES = {'default': ('en', ), 'en': ('pl', )}
+
+# Tutaj rejestruje się wszystkie trackery translacyjne.
+MODELTRANSLATION_TRANSLATION_FILES = (
+    'boards.translation',
+    'memories.translation',
+)
+# Konfiguracja CDN
+# https://github.com/jschneier/django-storages
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'fra1'
+
+AWS_S3_ENDPOINT_URL = 'https://esks.fra1.digitaloceanspaces.com'
+
+AWS_ACCESS_KEY_ID = 'UY25DNGAOVVGCA2UUK2L'
+
+AWS_SECRET_ACCESS_KEY = 'TtICSTYds/ipCuhUfmxZ2cpiV2sp+N+xR6Cp95dN1fw'
+
+AWS_STORAGE_BUCKET_NAME = 'app'
+
+AWS_DEFAULT_ACL = None
+
+# AWS_S3_CUSTOM_DOMAIN = 'https://esks.fra1.cdn.digitaloceanspaces.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'esks/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'  # Przekierowanie usera po zalogowaniu
+
+LOGOUT_REDIRECT_URL = '/'  # Przekierowanie po wylogowaniu.
+
+# Poniżej dane do ustawień Middleware blokującego strony przed niezalogowanymi.
+LOGIN_URL = '/'
+
+
+# https://github.com/CleitonDeLima/django-login-required-middleware
+LOGIN_REQUIRED_IGNORE_PATHS = [
+     r'^$',
+     r'^static/.*$',
+     r'^media/.*$',
+     r'^admin/.*$',
+]
+
+
+# Mailing Django. Do podmianki w local_settings.py
+# https://www.tutorialspoint.com/django/django_sending_emails.htm
+# https://docs.djangoproject.com/en/2.2/topics/email/
+EMAIL_HOST = 'localhost'
+
+EMAIL_HOST_USER = ''
+
+EMAIL_HOST_PASSWORD = ''
+
+EMAIL_PORT = 25
+
+EMAIL_USE_SSL = False
+
+EMAIL_SSL_CERTFILE = None
+
+# FILE_UPLOAD_HANDLERS = []
+
+# Ściągnij ustawienia lokalne gdybyśmy chcieli udostępnić kod i wejść na OpenSource
+# na serwerze obok "settings" robisz plik .local_settings i ustalasz od nowa:
+# SECRET_KEY, DEBUG = False, DATABASES, oraz CACHES jeśli używasz Memccache.
+# edit: oraz inne o których było wspomniane wcześniej.
+try:
+    from .local_settings import *
+except ImportError:
+    pass
